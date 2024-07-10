d5c18eacc4fa5e520b875cfa5bac3b3464c8afd6
08xmt <markus.08xmt@gmail.com>
2024-07-10 08:54:43 +0200

    Add arbitrage tests

--- src/simpleArb.sol
+++ src/simpleArb.sol
@@ -4,7 +4,7 @@ import {sINV} from "src/sInv.sol";
 
 interface ICurvePool {
     function exchange(uint256 i, uint256 j, uint256 dx, uint256 min_dy) payable external returns(uint256);
-    function get_dy(uint256 i, uint256 j, uint256 dx) external returns(uint256);
+    function get_dy(uint256 i, uint256 j, uint256 dx) external view returns(uint256);
 }
 interface IERC20 {
     function approve(address to, uint amount) external;
@@ -12,7 +12,7 @@ interface IERC20 {
     function balanceOf(address holder) external view returns(uint);
 }
 
-contract simpleArb {
+contract SimpleArb {
     ICurvePool public constant dbrTriPool = ICurvePool(0xC7DE47b9Ca2Fc753D6a2F167D8b3e19c6D18b19a);
     ICurvePool public constant invTriPool = ICurvePool(0x5426178799ee0a0181A89b4f57eFddfAb49941Ec);
     sINV public immutable sInv;
@@ -31,39 +31,55 @@ contract simpleArb {
         inv.approve(address(invTriPool), type(uint).max);
         inv.approve(address(sInv), type(uint).max);
         dbr.approve(address(dbrTriPool), type(uint).max);
+        weth.approve(address(invTriPool), type(uint).max);
     }
 
     function arb(uint amountWeth, uint expectedRevenue) external returns(uint wethOut){
         uint invIn = invTriPool.exchange(wethIndex, invIndex, amountWeth, 0);
         uint dbrOut = getDbrOut(invIn);
-        sInv.buyDBR(invIn, dbrOut, msg.sender);
+        sInv.buyDBR(invIn, dbrOut, address(this));
         uint invBal = dbrTriPool.exchange(dbrIndex, invIndex, dbrOut, 0);
         wethOut = invTriPool.exchange(invIndex, wethIndex, invBal, expectedRevenue);
     }
 
     function flashArb(uint invIn, uint expectedRevenue) external returns(uint wethOut){
         uint dbrOut = getDbrOut(invIn);
-        sInv.flashBuyDBR(invIn, dbrOut, msg.sender, abi.encode(invIn)); //Will execute flashSwapCallback
+        sInv.flashBuyDBR(invIn, dbrOut, address(this), abi.encode(invIn)); //Will execute flashSwapCallback
         uint invBal = inv.balanceOf(address(this));
         wethOut = invTriPool.exchange(invIndex, wethIndex, invBal, expectedRevenue);
     }
     
-    function getRevenue(uint amountWeth) public returns(uint){
+    function getRevenue(uint amountWeth) public view returns(int){
         uint invIn = invTriPool.get_dy(wethIndex, invIndex, amountWeth);
         uint dbrOut = getDbrOut(invIn);
-        sInv.buyDBR(invIn, dbrOut, msg.sender);
         uint invBal = dbrTriPool.get_dy(dbrIndex, invIndex, dbrOut);
         uint wethOut = invTriPool.get_dy(invIndex, wethIndex, invBal);       
-        if(wethOut < amountWeth) return 0;
-        return wethOut - amountWeth;
+        return int(wethOut) - int(amountWeth);
     }
 
-    function binaryRevenueSearch(uint minStep) external returns (uint, uint){
+    function arbInv(uint invIn, uint expectedRevenue) external returns(uint invBal){
+        uint dbrOut = getDbrOut(invIn);
+        sInv.buyDBR(invIn, dbrOut, address(this));
+        invBal = dbrTriPool.exchange(dbrIndex, invIndex, dbrOut, 0);
+    }
+
+    function flashArbInv(uint invIn, uint expectedRevenue) external {
+        uint dbrOut = getDbrOut(invIn);
+        sInv.flashBuyDBR(invIn, dbrOut, address(this), abi.encode(invIn)); //Will execute flashSwapCallback
+    }
+
+    function getRevenueInv(uint invIn) public view returns(int){
+        uint dbrOut = getDbrOut(invIn);
+        uint invBal = dbrTriPool.get_dy(dbrIndex, invIndex, dbrOut);
+        return int(invBal) - int(invIn);
+    }
+
+    function binaryRevenueSearch(uint minStep) external view returns (uint, int){
         uint amount = weth.balanceOf(address(this)) / 2; //Start binary search halfway. Max trade amount will be weth balance - minStep
-        uint step = amount / 2;
+        uint step = uint(amount) / 2;
         while(step > minStep){
-            uint inc = getRevenue(uint(amount + step));
-            uint dec = getRevenue(uint(amount - step));
+            int inc = getRevenue(amount + step);
+            int dec = getRevenue(amount - step);
             if(inc > dec) {
                 amount += step;
             } else {
@@ -74,13 +90,17 @@ contract simpleArb {
         return (amount, getRevenue(amount));
     }
 
-    function flashSwapCallback(bytes calldata data) external {
+    function flashSwapCallback(bytes calldata data) external virtual {
         require(msg.sender == address(sInv));
         uint dbrBal = dbr.balanceOf(address(this));
         dbrTriPool.exchange(dbrIndex, invIndex, dbrBal, 0);
         uint invOwed = abi.decode(data, (uint));
         //Pay off invOwed
         inv.transfer(address(sInv), invOwed);
+    }
+
+    function wethToInv(uint amountWeth) external view returns(uint invOut) {
+        invOut = invTriPool.get_dy(wethIndex, invIndex, amountWeth);
     }
 
     function getDbrOut(uint invIn) public view returns (uint dbrOut) {
@@ -92,4 +112,16 @@ contract simpleArb {
     }
 
 
+}
+
+contract EvilArb is SimpleArb{
+
+    constructor(address _sInv) SimpleArb(_sInv){
+    
+    }
+
+    function flashSwapCallback(bytes calldata data) external override {
+        require(msg.sender == address(sInv));
+        //Do nothing
+    }
 }
